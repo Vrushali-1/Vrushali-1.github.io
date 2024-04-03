336b3b6afbc167e1b8ee9676e44d1c576d67b463
Vrushali Patil <ek80970@umbc.edu>
2024-04-02 23:36:29 -0400

    CV uploaded

--- components/intro.tsx
+++ components/intro.tsx
@@ -93,7 +93,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="https://drive.google.com/file/d/12q6WXX1GOnsgECMlDTxmuAVUV5gMTcZG/view?usp=sharing"
+          href="https://drive.google.com/file/d/1qZzh3S_7TwHNmZ2bi2je4WpP4QKoE803/view"
           target="_blank"
           download
         >
