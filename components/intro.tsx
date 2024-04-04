830cce743b8790e8e97454f93a321b7d3208520c
Vrushali Patil <ek80970@umbc.edu>
2024-04-03 21:16:28 -0400

    link updated

--- components/intro.tsx
+++ components/intro.tsx
@@ -93,7 +93,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="https://drive.google.com/file/d/1qZzh3S_7TwHNmZ2bi2je4WpP4QKoE803/view"
+          href="https://drive.google.com/file/d/1lg0kFHkL4rE025dRgPx_UjnOUpluVM2H/view?usp=drive_link"
           target="_blank"
           download
         >
